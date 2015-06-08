5f0641cf21b355096ca14c1d5d50e015526a0d2a
Ryota Saito <rsait001@well.cs.ucr.edu>
2015-06-07 21:35:06 -0700

    updated git ignore, removed sql stuff from beingpushed

--- sql/scripts/create_db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-psql -p $PGPORT $DB_NAME < $DIR/../src/create_tables.sql
-psql -p $PGPORT $DB_NAME < $DIR/../src/create_indexes.sql
-psql -p $PGPORT $DB_NAME < $DIR/../src/load_data.sql
